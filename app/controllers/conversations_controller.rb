13c6494b89de8aac0a21e679589157a6f8be31b3
Elizabeth Alexander & Frank Lam <pair+eealexander2.ftlam11@devbootcamp.com>
2016-05-26 17:27:26 -0500

    Refine basic messaging feature

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -6,6 +6,7 @@ class ConversationsController < ApplicationController
     p conversations = @user.conversations
     p other_person_id = Conversation.get_sender_id(user_id, conversations )
     p @name = Conversation.sender_name(other_person_id)
+    @conversation = Conversation.first
   end
 
   def show
@@ -15,5 +16,35 @@ class ConversationsController < ApplicationController
 
   def new
     @message = Message.new
+  end
+
+  def create
+    # returns all conversations of the recipient
+    recipient = User.find_by(id: params[:user_id])
+    sender_id = session[:user_id]
+    conversations = User.find(recipient).conversations
+    conversation_id = Conversation.conversation_exist(conversations,sender_id)
+
+    if conversation_id
+      @message = Message.new(sender_id: session[:user_id], body: params[:message][:body], conversation_id: conversation_id)
+      if @message.save
+        redirect_to user_path(recipient)
+      else
+        render 'new'
+      end
+
+    else
+      new_conversation = Conversation.create()
+      @message = Message.new(sender_id: session[:user_id], body: params[:message][:body], conversation_id: new_conversation.id)
+
+      if @message.save
+        redirect_to user_path(recipient)
+      else
+        render 'new'
+      end
+    end
+
+    # if a conversation already exists with the recipient, then use that existing conversation_id
+    # else create a new Conversation object and add sender as the user_id, and add new conversation_id to current message
   end
 end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -21,4 +21,20 @@ class Conversation < ActiveRecord::Base
   def self.sender_name(other_person_id)
     return User.find(other_person_id).name
   end
+
+  def self.conversation_exist(conversations, sender_id)
+    # iterate over all the recipient's conversations
+    conversations.each do |conversation|
+    # for each conversation, look into message
+      conversation.messages.each do |message|
+    # for each message, check if the session_id is eql to the sender_id
+        if message.sender_id == sender_id
+    # if there is a match, return the conversation_id
+          return message.conversation_id
+        end
+      end
+    end
+    return nil
+  end
+
 end
