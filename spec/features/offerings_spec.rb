4ae5784e8d066ccb902f1f73d547b344b434d1f4
Frank Lam & Jonathan Kaplan <pair+ftlam11.jbkaplan@devbootcamp.com>
2016-05-25 19:19:31 -0500

    Add offering form

--- spec/features/offerings_spec.rb
+++ spec/features/offerings_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature "user sees index page of offerings" do
+  let!(:user){User.create(email: "dom@bob.com", password: "bob", name: "Bob")}
   scenario "user can click on button and offer to mentor" do
     visit '/'
     click_button 'Offer To Mentor'
@@ -9,8 +10,15 @@ feature "user sees index page of offerings" do
 
   scenario "user sees a form to select date and time slots " do
     visit '/'
+
+    # user.offerings.create(start_time: "9:00", end_time: "9:30")
     click_button 'Offer To Mentor'
-    expect(page).to have_css(".modal-content")
+    within('form') do
+      fill_in 'Start Time', with:"10:00"
+      fill_in 'End Time', with:"12:00"
+    end
+    click_button 'Submit'
+    expect(page).to have_css("#user1")
   end
 
 end
