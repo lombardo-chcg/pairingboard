bd016cdccf27746f80fbe2bb90dd937ea8385131
Frank Lam & Jonathan Kaplan <pair+ftlam11.jbkaplan@devbootcamp.com>
2016-05-25 20:03:45 -0500

    Playing with buttons.  end of day

--- app/controllers/offerings_controller.rb
+++ app/controllers/offerings_controller.rb
@@ -2,4 +2,14 @@ class OfferingsController < ApplicationController
   def index
     @offerings
   end
+
+  def create
+    current_user.offerings.create(offering_params)
+    redirect_to offerings_path
+  end
+
+  private
+    def offering_params
+      params.permit(:start_time, :end_time)
+    end
 end
