bbc23bddc0698ec449329aab2faac8c13ee66d4c
Elizabeth Alexander & Frank Lam <pair+eealexander2.ftlam11@devbootcamp.com>
2016-05-26 08:27:09 -0500

    Adds User Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
   has_many :offerings, foreign_key: :mentor_id
   has_many :appointments, foreign_key: :student_id
 
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: {minimum: 8}
+
+
   has_secure_password
 
   def student_appointments
