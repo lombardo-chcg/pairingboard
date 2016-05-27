ad05832d713180c98591f19b6ef9598db0389523
Elizabeth Alexander & Frank Lam <pair+eealexander2.ftlam11@devbootcamp.com>
2016-05-27 09:37:47 -0500

    Nuclear launch detected

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,17 +7,21 @@ class ConversationsController < ApplicationController
     user_id = User.find(session[:user_id]).id
     #get all conversations of current user
     conversations = @user.conversations
+    all = Converser.all
+    p all
+
+
     #go through all messages in a conversation until the recipient id is found, return the recipient id
-    other_person_id_array = Conversation.get_sender_id(user_id, conversations)
+    @senders_array = Conversation.get_sender(user_id, conversations)
     #using the recipient id, get the recipient's name
 
-    @other_person_array = Conversation.sender_name(other_person_id_array)
-    #get the conversation for use in the helper path
+    # @other_person_array = Conversation.sender_name(other_person_id_array)
+    p @senders_array
   end
 
   def show
-    conversation = Conversation.find(params[:id])
-    @messages = conversation.messages
+    @conversation = Conversation.find(params[:id])
+    @messages = @conversation.messages
   end
 
   def new
@@ -28,11 +32,18 @@ class ConversationsController < ApplicationController
     # returns all conversations of the recipient
     recipient = User.find(params[:user_id])
     sender_id = session[:user_id]
+
     conversations = recipient.conversations
-    puts "=================#{conversations.first.messages.last.body}============"
+    # puts "=================#{conversations.first.messages.last.body}============"
     conversation_id = Conversation.conversation_exist(conversations,sender_id)
+    # p conversation_id
+
+    # conversation_id = Conversation.conversation_exist(conversations,sender_id)
     if conversation_id
       @message = Message.new(sender_id: session[:user_id], body: params[:message][:body], conversation_id: conversation_id)
+      ###Conversation.find(conservation_id).conversers.create(user_id: XXXXX)
+      ###Currently, only the sender is tied to a message that is tied to a conversation
+      ###We should associate the recipient with a conversation through Converser
       if @message.save
         redirect_to user_path(recipient)
       else
@@ -40,11 +51,12 @@ class ConversationsController < ApplicationController
       end
 
     else
-      new_conversation = Conversation.new()
-      @message = Message.new(sender_id: session[:user_id], body: params[:message][:body], conversation_id: new_conversation.id)
-
+      new_conversation = Conversation.create()
+      # @message = Message.new(sender_id: session[:user_id], body: params[:message][:body], conversation_id: new_conversation.id)
+      @message = new_conversation.messages.new(sender_id: session[:user_id], body: params[:message][:body])
+###Conversation.find(conservation_id).conversers.create(user_id: XXXXX)
       if @message.save
-        new_conversation.save
+        # new_conversation.save
         redirect_to user_path(recipient)
       else
         render 'new'
