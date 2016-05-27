14b3e66559c1d73cc96e12b4410efec10eb8737d
jbkaplan <jbkaplan@outlook.com>
2016-05-27 17:41:49 -0500

    Update JS files to fix asset compile heroku error

--- app/assets/javascripts/offerings.js
+++ app/assets/javascripts/offerings.js
@@ -6,19 +6,19 @@ $(document).ready(function() {
     var request = $.ajax({
       method: "Post",
       url: "/offerings",
-      data: $(this).serialize(),
+      data: $(this).serialize()
     });
     request.done(function(response) {
-        $('.container').append(response)
+        $('.container').append(response);
         $(".flash_area").show();
         $(".flash_area").html("<div class='alert alert-success flash-alert'>Appointment Booked!</div>").fadeOut(3500);
         $('#myModal').modal('hide');
     });
     request.fail(function(error) {
-        console.log(error)
-        var errors = error.responseText
-        var message = JSON.parse(errors).message
-        var errorMessage = message.student[0]        
+        console.log(error);
+        var errors = error.responseText;
+        var message = JSON.parse(errors).message;
+        var errorMessage = message.student[0];    
         $('#myModal').modal('hide');
         $(".flash_area").show();
         $(".flash_area").html("<div class='alert alert-danger flash-alert'>"+errorMessage+"</div>").fadeOut(3500);
@@ -57,7 +57,7 @@ $(document).ready(function() {
       var request = $.ajax({
         method: "Post",
         url: "/appointments",
-        data: {time, user}
+        data: { time:time, user:user }
       });
       request.done(function(response){
         $(".flash_area").show();
@@ -66,10 +66,9 @@ $(document).ready(function() {
         $('#appointmentModal').modal('hide');
       });
      request.fail(function(error){
-        console.log(error)
-        var errors = error.responseText
-        var message = JSON.parse(errors).message
-        var errorMessage = message.student[0]        
+        var errors = error.responseText;
+        var message = JSON.parse(errors).message;
+        var errorMessage = message.student[0];    
         $('#appointmentModal').modal('hide');
         $(".flash_area").show();
         $(".flash_area").html("<div class='alert alert-danger flash-alert'>"+errorMessage+"</div>").fadeOut(3500);
@@ -83,22 +82,22 @@ $(document).ready(function() {
     var thisButton = $(this);
     var time = $(this).attr("value");
     var user = ($(this).parent().parent().find('button').attr('name'));
-    var appointmentId = $(this).attr('name')
+    var appointmentId = $(this).attr('name');
     var request = $.ajax({
       method: "Get",
       url: "/appointments/"+appointmentId,
-      data: {time, user}
+      data: { time:time, user:user }
     });
     request.done(function(response) {
-      $('.show-mentor-name').html('Mentor Name: ' + response.mentor)
-      $('.show-student-name').html('Student Name: ' + response.student)
-      $('.show-appointment-time').html('Time: ' + time)
+      $('.show-mentor-name').html('Mentor Name: ' + response.mentor);
+      $('.show-student-name').html('Student Name: ' + response.student);
+      $('.show-appointment-time').html('Time: ' + time);
       $('#show-appointment-modal').modal();
-      if (response.appointmentHolder == true ) {
+      if (response.appointmentHolder === true ) {
         $('.show-appointment-cancel').html("<button type='button' class='btn btn-info btn-lg cancel-appointment' name="+response.appointmentId+">Cancel Appointment</button>");
       } else {
         $('.show-appointment-cancel').html('');
-      };
+      }
     });
   });
 
@@ -113,7 +112,6 @@ $(document).ready(function() {
         url: "/appointments/"+appointmentId
       });
       request.done(function(response) {
-        console.log(response)
         $(".flash_area").show();
         $(".flash_area").html("<div class='alert alert-danger flash-alert'>Appointment Cancelled!</div>").fadeOut(3500);
         $("a[href='/appointments/"+response.appointmentId+"']").removeClass('taken_offering_link').addClass('open_offering_link').attr('href', "/offerings/"+response.offeringId);
@@ -123,31 +121,30 @@ $(document).ready(function() {
 
     // click on user name and see profile modal
     $('body').on('click', '.name_button', function(event) {
-      userId = $(this).attr('name')
+      userId = $(this).attr('name');
       request = $.ajax({
         method: "GET",
         url:  "/users/"+userId
       });
 
       request.done(function(response) {
-        console.log(response)
-        var name = response.userName
-        $('.mentor-title').html(response.userName)
+        var name = response.userName;
+        $('.mentor-title').html(response.userName);
         if (response.userPhase === null) {
-           $('.show-mentor-phase').html("No phase provided")
+           $('.show-mentor-phase').html("No phase provided");
         }else {
-          $('.show-mentor-phase').html(response.userPhase)
+          $('.show-mentor-phase').html(response.userPhase);
         }
         if(response.userPhone === null ){
-           $('.show-mentor-phone').html("Sorry, no phone number provided!")
+           $('.show-mentor-phone').html("Sorry, no phone number provided!");
         }else{
-          $('.show-mentor-phone').html(response.userPhone)
+          $('.show-mentor-phone').html(response.userPhone);
         }if(response.userDescription === null ){
-          $('.show-mentor-description').html("No story provided")
+          $('.show-mentor-description').html("No story provided");
         }else{
-          $('.show-mentor-description').html(response.userDescription)
+          $('.show-mentor-description').html(response.userDescription);
         }
-        $('.link-to-profile').html("<a class='btn btn-default user-profile' href='/users/" + userId +"'>"+ name +"profile </a>")
+        $('.link-to-profile').html("<a class='btn btn-default user-profile' href='/users/" + userId +"'>"+ name +"profile </a>");
         $('#show-user-modal').modal();
 
       });
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(email: "liz@bob", password: "aaaaaaaa", name: "liz", phone_number: "1234567890")
-User.create(email: "fronk@bob", password: "aaaaaaaa", name: "fronk", phone_number: "1234567890")
-User.create(email: "dom@bob", password: "mmmmmmmm", name: "dom", phone_number: "91234567")
+user = User.create(email: "liz@bob", password: "aaaaaaaa", name: "liz", phone_number: "1234567890", current_phase: 4)
+user2 = User.create(email: "fronk@bob", password: "aaaaaaaa", name: "fronk", phone_number: "1234567890", current_phase: 3)
+user3 = User.create(email: "dom@bob", password: "mmmmmmmm", name: "dom", phone_number: "91234567", current_phase: 2)
 
-Conversation.create()
-Conversation.create()
 
-Converser.create(user_id: 1, conversation_id: 1)
-Converser.create(user_id: 2, conversation_id: 1)
-Converser.create(user_id:1, conversation_id:2)
-Converser.create(user_id:3, conversation_id:1)
+user.offerings.create(offering_date: "28-05-2016", start_time: "9:00")
+user.offerings.create(offering_date: "28-05-2016", start_time: "9:30")
+user.offerings.create(offering_date: "28-05-2016", start_time: "10:00")
+user.offerings.create(offering_date: "28-05-2016", start_time: "10:30")
+user.offerings.create(offering_date: "28-05-2016", start_time: "11:00")
+user.offerings.create(offering_date: "28-05-2016", start_time: "11:30")
+user.offerings.create(offering_date: "28-05-2016", start_time: "12:00")
+user.offerings.create(offering_date: "28-05-2016", start_time: "12:30")
+user.offerings.create(offering_date: "28-05-2016", start_time: "1:00")
 
-Message.create(conversation_id: 1, body: "Hello", sender_id: 1)
-Message.create(conversation_id: 1, body: "HAIIIII", sender_id: 2)
-Message.create(conversation_id: 2, body: "HEY GURL", sender_id: 3)
+
+# Conversation.create()
+# Conversation.create()
+
+# Converser.create(user_id: 1, conversation_id: 1)
+# Converser.create(user_id: 2, conversation_id: 1)
+# Converser.create(user_id:1, conversation_id:2)
+# Converser.create(user_id:3, conversation_id:1)
+
+# Message.create(conversation_id: 1, body: "Hello", sender_id: 1)
+# Message.create(conversation_id: 1, body: "HAIIIII", sender_id: 2)
+# Message.create(conversation_id: 2, body: "HEY GURL", sender_id: 3)
