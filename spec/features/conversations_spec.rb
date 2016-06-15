566522e3e5d55a0553c2ccc30f6909f408d369ce
Dominick <dpa.lombardo@gmail.com>
2016-06-15 03:42:44 +0000

    Update conversation spec file

--- spec/features/conversations_spec.rb
+++ spec/features/conversations_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 feature "user can view index page of all their conversations" do
   let!(:dom){User.create(email: "dom@bob.com", password: "aaaaaaaa", name: "Dom")}
   let!(:bob){User.create(email: "bob@bob.com", password: "aaaaaaaa", name: "Bob")}
-  let!(:conversation){Conversation.create()}
-  let!(:dom_talk){Converser.create(user_id: 1, conversation_id: 1)}
-  let!(:bob_talk){Converser.create(user_id: 1, conversation_id: 2)}
+  let!(:conversation){Conversation.create(creator_id: 1, joiner_id: 2)}
   let!(:bob_message){Message.create(sender_id: 2, body: "pineapple", conversation_id: 1)}
 
   scenario "user can click on Inbox in the nav bar to see list of conversations" do
